353c69b9290669758a2da408b443490addce26d7
Chris Grocott <chris.grocott@gmail.com>
2017-03-17 08:10:06 +0000

    added code to see the effect of varying C parameter with SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -29,14 +29,33 @@ features_train = features_train[:len(features_train)/100]
 labels_train = labels_train[:len(labels_train)/100]
 
 from sklearn.svm import SVC
-clf = SVC(kernel="rbf")
-t0 = time()
-clf.fit(features_train, labels_train)
-print "training time:", round(time()-t0, 3), "s"
-t0 = time()
-accuracy = clf.score(features_test, labels_test)
-print "predicting time:", round(time()-t0, 3), "s"
-print accuracy
+#clf = SVC(kernel="rbf")
+
+def run_test(clf):
+  t0 = time()
+  clf.fit(features_train, labels_train)
+  print "training time:", round(time()-t0, 3), "s"
+  t0 = time()
+  accuracy = clf.score(features_test, labels_test)
+  print "predicting time:", round(time()-t0, 3), "s"
+  print "accuracy:", accuracy
+  print "=========================================="
+
+print "Training with c=10.0"
+run_test(SVC(C=10.0, kernel="rbf"))
+
+print "Training with c=100.0"
+run_test(SVC(C=100.0, kernel="rbf"))
+
+print "Training with c=1000.0"
+run_test(SVC(C=1000.0, kernel="rbf"))
+
+print "Training with c=10000.0"
+run_test(SVC(C=10000.0, kernel="rbf"))
+
+#print "Training with c=10.0"
+#run_test(SVC(c=10.0, kernel="rbf"))
+
 #########################################################
 
 
